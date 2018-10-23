026b6ef8a85551d96a618832d392d3428d457374
Dmitry Lobanov <d.lobanov@itransition.com>
2018-10-23 23:25:40 +0300

    Update setup command

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Build and run docker
+docker-compose down
+docker-compose build
+docker-compose up -d
+
 # Composer install
 docker exec -it bouncer.php php composer.phar install
 
@@ -11,6 +16,7 @@ docker exec -it bouncer.php php artisan key:generate
 
 # Init database
 touch storage/database.sqlite
+chmod 0777 storage/database.sqlite
 docker exec -it bouncer.php php artisan migrate
 
 printf "\e[33mAttention:\e[0m change MICROSOFT_APP_ID and MICROSOFT_APP_KEY in .env\n"
