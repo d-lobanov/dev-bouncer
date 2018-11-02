7266c9b5ebbfa73f4ecb188095207649e686a0d8
Dmitry Lobanov <dmitrylo@playtika.com>
2018-11-02 20:05:08 +0300

    Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,7 +6,7 @@ docker-compose -f docker-compose.prod.yml build
 docker-compose -f docker-compose.prod.yml up -d
 
 echo "Composer install"
-docker exec -t prod.php php composer.phar install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
+docker exec -t prod.php php composer.phar install --no-ansi --no-dev --no-interaction --no-progress --optimize-autoloader
 
 if [ ! -f .env ]; then
     echo "Init env"
@@ -15,3 +15,7 @@ if [ ! -f .env ]; then
     echo "Generate key"
     docker exec -t prod.php php artisan key:generate
 fi
+
+echo "Change permission"
+docker exec -t prod.php chmod -R 0777 storage
+docker exec -t prod.php chmod -R 0777 bootstrap/cache
