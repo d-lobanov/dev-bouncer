5ba39a31978f9f5103be160e18190f3bb1b271f5
Dmitry Lobanov <dmitrylo@playtika.com>
2018-11-02 19:28:30 +0300

    Add deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,15 +6,15 @@ docker-compose -f docker-compose.prod.yml build
 docker-compose -f docker-compose.prod.yml up -d
 
 # Composer install
-docker exec -it prod.php php composer.phar install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
+docker exec -t prod.php php composer.phar install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
 
 if [ ! -f .env ]; then
     # Init env
     cp .env.example .env
 
     # Generate key
-    docker exec -it prod.php php artisan key:generate
+    docker exec -t prod.php php artisan key:generate
 fi
 
 # Run migration
-docker exec -it prod.php php artisan migrate --force
+docker exec -t prod.php php artisan migrate --force
