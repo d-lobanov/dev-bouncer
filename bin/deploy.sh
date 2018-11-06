b36821e65cdf87c612a48a756014fd67426168c4
Dmitry Lobanov <dmitrylo@playtika.com>
2018-11-06 13:16:47 +0300

    Add deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,21 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Build and run docker"
-docker-compose down
-docker-compose -f docker-compose.prod.yml build
-docker-compose -f docker-compose.prod.yml up -d
-
-echo "Composer install"
-docker exec -t prod.php php composer.phar install --no-ansi --no-dev --no-interaction --no-progress --optimize-autoloader
-
-if [ ! -f .env ]; then
-    echo "Init env"
-    cp .env.example .env
-
-    echo "Generate key"
-    docker exec -t prod.php php artisan key:generate
-fi
-
-echo "Change permission"
-docker exec -t prod.php chmod -R 0777 storage
-docker exec -t prod.php chmod -R 0777 bootstrap/cache
+docker-compose pull
+docker-compose up -d
+docker exec -it prod.php php artisan config:cache
+docker image prune -a -f
